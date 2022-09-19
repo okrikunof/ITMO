7a6b0df2e8d3a5a1814fee0f72a0b68dd5f30259
Krikunov Oleg <113013274+okrikunof@users.noreply.github.com>
2022-09-19 18:35:16 +0300

    Update main.c

--- LAB №2/main.c
+++ LAB №2/main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-    char str;
-    float real_num;
-    printf("Введите строку:\n");
-    scanf("%s", &str);
-    printf("Введите вещественное число:\n");
-    scanf("%f", &real_num);
-    printf("Результат: %s, %g", &str, real_num);
+    float z1, z2, a;
+    printf("Введите значение угла в градусах:\n");
+    scanf("%f", &a);
+    z1 = (1 - 2 * powf(sinf(a), 2)) / (1 + sinf(2 * a));
+    z2 = (1 - tanf(a)) / (1 + tanf(a));
+    printf("Результат вычислений в радианах: z1 = %f, z2 = %f", z1, z2);
     return 0;
 }
