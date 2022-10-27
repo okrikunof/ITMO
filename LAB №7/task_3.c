a3b52e861a8a5adf186b774cc99abe11605a126a
Krikunov Oleg <113013274+okrikunof@users.noreply.github.com>
2022-10-27 14:51:09 +0300

    Update and rename task_3 to task_3.c

---
+++ LAB №7/task_3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+union {
+    struct {
+        int num_lock : 1;
+        int caps_lock : 1;
+        int scroll_lock : 1;
+    };
+    char status;
+}code;
+
+int main() {
+    scanf("%x", &code.status);
+
+    if (code.num_lock)
+        printf("num_lock is on\n");
+    else
+        printf("num_lock is off\n");
+
+    if (code.caps_lock)
+        printf("caps_lock is on\n");
+    else
+        printf("caps_lock is off\n");
+
+    if (code.scroll_lock)
+        printf("scroll_lock is on\n");
+    else
+        printf("scroll_lock is off\n");
+
+    return 0;
+}
